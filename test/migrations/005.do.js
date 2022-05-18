092c81b58103e7fdee70ff5decc9336e3ffdc433
Julian Grinblat <julian@dotcore.co.il>
2022-05-19 00:26:14 +0900

    Add eslint-plugin-unicorn

--- test/migrations/005.do.js
+++ test/migrations/005.do.js
@@ -1,5 +1,6 @@
 /* eslint-disable func-names */
 
+// eslint-disable-next-line unicorn/prefer-module
 module.exports.generateSql = function () {
     return "INSERT INTO person (name, age) VALUES ('molly', 50);";
 };
