a9ef7b73a89fba15db0f9d18672121a04fc0fc54
Julian Grinblat <julian@dotcore.co.il>
2022-06-06 10:25:40 +0900

    Add eslint-plugin-eslint-commens

--- test/migrations/005.do.js
+++ test/migrations/005.do.js
@@ -1,6 +1,2 @@
-/* eslint-disable func-names */
-
 // eslint-disable-next-line unicorn/prefer-module,no-undef
-module.exports.generateSql = function () {
-    return "INSERT INTO person (name, age) VALUES ('molly', 50);";
-};
+module.exports.generateSql = () => "INSERT INTO person (name, age) VALUES ('molly', 50);";
--- test/migrations/005.undo.js
+++ test/migrations/005.undo.js
@@ -1,6 +1,2 @@
-/* eslint-disable func-names */
-
 // eslint-disable-next-line unicorn/prefer-module,no-undef
-module.exports.generateSql = function () {
-    return "DELETE FROM person WHERE name = 'molly'";
-};
+module.exports.generateSql = () => "DELETE FROM person WHERE name = 'molly'";
