44a95b922bf3af564d24a529d94361e76aa73da8
Jhon Mosk <40711760+Jhon-Mosk@users.noreply.github.com>
2025-09-21 10:35:03 +0300

    feat: support multiple patterns (#300)
    * feat: support multiple patterns
    
    * test: Add tests for multi patterns config

---
+++ test/seeds/006.do.some-description.sql
@@ -0,0 +1,5 @@
+-- Test 1 insert
+INSERT INTO
+    person (name, age)
+VALUES
+    ('jhon', 30);
---
+++ test/seeds/006.undo.some-description.sql
@@ -0,0 +1,6 @@
+-- remove the record
+DELETE FROM
+    person
+WHERE
+    name = 'jhon'
+    AND age = 30;
