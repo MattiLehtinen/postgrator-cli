df0752c7e3143a2a8c0ca12882d1cb59c967f55f
Julian Grinblat <julian@dotcore.co.il>
2026-07-15 04:04:48 +0900

    chore(linter): format yaml files

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import imprt from "eslint-plugin-import";
 import unicorn from "eslint-plugin-unicorn";
 import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
 import prettier from "eslint-plugin-prettier/recommended";
+import { configs as yml } from "eslint-plugin-yml";
 
 export default [
     js.configs.recommended,
@@ -11,6 +12,7 @@ export default [
     comments.recommended,
     unicorn.configs.recommended,
     imprt.flatConfigs.recommended,
+    ...yml.recommended,
     prettier,
     {
         languageOptions: {
@@ -24,6 +26,8 @@ export default [
             "unicorn/no-useless-promise-resolve-reject": "off",
             "unicorn/catch-error-name": ["error", { name: "e" }],
             "@eslint-community/eslint-comments/no-unused-disable": "error",
+            // GitHub Actions workflows rely on empty mapping values, e.g. `pull_request:`
+            "yml/no-empty-mapping-value": "off",
         },
     },
     {
